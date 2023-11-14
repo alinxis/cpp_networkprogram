9a421de0b167fc6639153877d8d154817f8ca199
Linsux <1932238665@qq.com>
2023-11-14 20:16:40 +0800

    重构实现InetAddress类

--- include/InetAddress.h
+++ include/InetAddress.h
@@ -4,5 +4,18 @@
 
 #ifndef CPPNET_INETADDRESS_H
 #define CPPNET_INETADDRESS_H
+#include <arpa/inet.h>
+#include <cstring>
+
+class InetAddress
+{
+public:
+    struct sockaddr_in m_addr;
+    socklen_t m_addr_len=socklen_t(sizeof (sockaddr_in));//c++ 17起才支持，支持低版本请使用初始化列表
+
+    InetAddress(){ bzero(&m_addr,sizeof(struct sockaddr_in)) ;}
+    InetAddress(const char* ip, uint16_t port);
+    ~InetAddress()=default;//目前使用编译器生成的就行
+};
 
 #endif //CPPNET_INETADDRESS_H
--- src/InetAddress.cc
+++ src/InetAddress.cc
@@ -1,3 +1,14 @@
 //
 // Created by King on 2023/11/14.
 //
+
+#include "InetAddress.h"
+
+InetAddress::InetAddress(const char *ip, uint16_t port) {
+    //初始化结构体
+    bzero(&m_addr,sizeof(struct sockaddr_in));
+    m_addr.sin_family=AF_INET;
+    m_addr.sin_port= htons(port);
+    m_addr.sin_addr.s_addr= inet_addr(ip);
+
+}
