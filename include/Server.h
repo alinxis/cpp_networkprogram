ec88cf879e11a9b9c8862f620ad67d540ea63f8b
alinxis <sun18232205091@126.com>
2023-11-30 15:16:36 +0800

    重写了server

--- include/Server.h
+++ include/Server.h
@@ -6,21 +6,25 @@
 #define CPPNET_SERVER_H
 
 #include <memory>
+#include <map>
 
 class EventLoop;
 class Socket;
 class Acceptor;
+class Connection;
 class Server
 {
 private:
      std::shared_ptr<EventLoop> m_loop;
      std::unique_ptr<Acceptor> m_acceptor;
+     std::map<int,Connection*> m_connections;
 public:
 
     explicit Server(std::shared_ptr<EventLoop>&);
     ~Server()=default;
 
     void handleReadEvent(int);
-    void newConnection(Socket *serv_sock);
+    void newConnection(Socket *sock);
+    void deleteConnection(Socket *sock);
 };
 #endif //CPPNET_SERVER_H
--- src/Server.cc
+++ src/Server.cc
@@ -11,8 +11,7 @@
 #include "Socket.h"
 #include "error.h"
 #include "Acceptor.h"
-#define BUFFER_SIZE 1024
-
+#include "Connection.h"
 Server::Server(EventLoop::shared_ptr& ep ):m_loop(ep) {
 
     m_acceptor=std::make_unique<Acceptor>(ep);
@@ -26,47 +25,18 @@ Server::Server(EventLoop::shared_ptr& ep ):m_loop(ep) {
 
 
 void Server::handleReadEvent(int fd){
-    char buffer[BUFFER_SIZE];
-    while(true) {    //由于使用非阻塞IO，读取客户端buffer，一次读取buf大小数据，直到全部读取完毕
-        bzero(&buffer, sizeof(buffer));
-        ssize_t recv_len = read(fd, buffer, sizeof(buffer));
-        if (recv_len > 0) {
-            printf("[Msg] message from client fd %d: %s\n", fd, buffer);
-            write(fd, buffer, sizeof(buffer));
-            continue;
-        }
-        if (recv_len == -1 && errno == EINTR) {  //客户端正常中断、继续读取
-            printf("[info] continue reading");
-            continue;
-        }
-        if (recv_len == -1 && ((errno == EAGAIN) || (errno == EWOULDBLOCK))) {//非阻塞IO，这个条件表示数据全部读取完毕
-            printf("[info] finish reading once, errno: %d\n", errno);
-            break;
-        }
-        if (recv_len == 0) {  //EOF，客户端断开连接
-            printf("EOF, client fd %d disconnected\n", fd);
-            close(fd);   //关闭socket会自动将文件描述符从epoll树上移除
-            break;
-        }
-    }
+
 }
 
-void Server::newConnection(Socket *serv_sock) {
+void Server::newConnection(Socket *sock) {
+    Connection* conn=new Connection(m_loop,sock);
+    m_connections[sock->getFd()]=conn;//添加conn
+    std::function<void(Socket*)> func= std::bind(&Server::deleteConnection,this,std::placeholders::_1);
+    conn->setDeleteConnectionCallback(func);
+}
 
-    InetAddress* client_addr= new InetAddress();
-
-    std::cout<<"[INFO] New Client From Ip:"<<inet_ntoa(client_addr->m_addr.sin_addr)<<" "
-            <<"Port: "<< ntohs(client_addr->m_addr.sin_port);
-
-    Socket client_socket=Socket(serv_sock->accept(client_addr));
-    std::cout<<"Socket Fd: "<<client_socket.getFd()<<std::endl;
-
-
-    client_socket.setnonblocking();
-    Channel *client_chan=new Channel(m_loop,client_socket.getFd());
-    std::function<void()> func=std::bind(&Server::handleReadEvent,this,client_socket.getFd());
-    client_chan->setCallBack(func);
-    Debugf("Client channel enableReading", __PRETTY_FUNCTION__);
-    client_chan->enableReading();
-    delete client_addr;
+void Server::deleteConnection(Socket * sock){
+    Connection *conn = m_connections[sock->getFd()];
+    m_connections.erase(sock->getFd());
+    delete conn;
 }
